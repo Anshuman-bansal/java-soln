2a4fe1a45d9e6ad2fd5b74641501ba528de9a5e5
objectgrind <55782576+objectgrind@users.noreply.github.com>
2020-09-16 22:01:23 +0530

    fifth commit
    fifth commit

---
+++ java8.java
@@ -0,0 +1,24 @@
+//hackerrank
+// java string reverse problem
+//solution
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+   public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String result ="Yes";
+        int length = A.length();
+        for (int i=0; i<length/2;i++){
+            if(A.substring(i,i+1).equals(A.substring(length-i-1,length-i))) {         
+            }else {
+             result ="No";
+            }
+        }
+        System.out.println(result);
+    }
+}
