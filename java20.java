6522be08ca4676ecad8a3f113f61cd7b4ce252ba
objectgrind <55782576+objectgrind@users.noreply.github.com>
2020-09-25 23:28:25 +0530

    14 commit

---
+++ java20.java
@@ -0,0 +1,43 @@
+//hackerrank
+//java bigdecimal problem
+//solution
+import java.math.BigDecimal;
+import java.util.*;
+class Solution{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+
+    for(int i=0;i<n;i++)
+        {
+            BigDecimal max=new BigDecimal(s[i]);
+            int index=i;
+            for(int j=i+1;j<n;j++)
+            {
+                BigDecimal curr=new BigDecimal(s[j]);
+                if(curr.compareTo(max)==1)
+                {
+                    max=curr;
+                    index=j;
+                }   
+            }
+            String temp=s[i];
+            s[i]=s[index];
+            s[index]=temp;
+        }          
+
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}
